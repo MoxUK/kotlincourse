516d1ade545c3b0b9b55282d8ccee8b247773f5a
Mox <soerendiversen@gmail.com>
2025-10-01 22:32:10 +0200

    Stable

---
+++ src/main/kotlin/module1/chapter5_collections/util/Calculator.kt
@@ -0,0 +1,4 @@
+package module1.chapter5_collections.util
+
+class Calculator {
+}
---
+++ src/main/kotlin/module1/chapter5_collections/util/Sample.kt
@@ -0,0 +1,7 @@
+package module1.chapter5_collections.util
+
+class Sample() {
+    fun sum(a: Int, b:Int): Int {
+        return a + b
+    }
+}

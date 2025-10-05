e9390758a6373db11bf1603a075ee96b993a91ff
Mox <soerendiversen@gmail.com>
2025-10-05 16:49:37 +0200

    StringUtilTest added to module2 test

--- src/test/kotlin/module1/chapter5_collections/utils/StringUtilTest.kt
+++ src/test/kotlin/module1/chapter5_collections/utils/StringUtilTest.kt
@@ -1,4 +1,22 @@
-package module1.chapter5_collections.utils
+//package module1.chapter5_collections.utils
+package module2.util
+
+import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.Test
+
 
 class StringUtilTest {
+
+    /* getText()*/
+    @Test
+    fun testGetTextLength()  {
+        val text = "Kotlin"
+        val expected = 6
+        assertEquals(6, text.length)
+    }
+
+    fun testGetTextNotNull() {
+        val str: String = "Hello"
+        assertNotNull(str)
+    }
 }
